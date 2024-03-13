f6d0e2c40deb51a09cf660a77bbf0fddcb4b539c
E X <firelifeeeeee@gmail.com>
2024-03-13 20:07:57 +0400

    project

--- calculator/Form1.Designer.cs
+++ calculator/Form1.Designer.cs
@@ -111,9 +111,8 @@ namespace ConnectionAPIGUI
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(12, 156);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 8;
-            this.label3.Text = "label3";
             this.label3.Click += new System.EventHandler(this.label3_Click_1);
             // 
             // Form1
